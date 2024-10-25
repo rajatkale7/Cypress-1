d773b3e14a3c42750a41a21133f44215e63446de
rajatkale7 <rajatkale7@gmail.com>
2024-10-26 02:25:40 +0530

    added fixture data

---
+++ cypress/e2e/usingFixtureData.cy.js
@@ -0,0 +1,20 @@
+describe('This is a Sauce-Demo website', () =>{
+
+    beforeEach('Performs the login', () =>{
+        cy.clearCookies();
+        cy.fixture('sauceDemoLogin').as('userData');  //From fixtures folder, store the data as 'userData'
+    })
+
+    it('Performs the login', function(){
+        const userKeys= ['user1', 'user2', 'user3', 'user4', 'user5', 'user6', 'user7', 'user8'];   //Keys of the login credentials, refer sauceDemoLogin.json folder for clarity
+
+        userKeys.forEach((key) => {
+            const user= this.userData[key];   //store the value for each key
+            cy.visit('https://www.saucedemo.com/v1/');
+            cy.xpath('//div/form/input[1]').type(user.userName);
+            cy.xpath('//div/form/input[2]').type(user.password);
+            cy.xpath('//div/form/input[3]').click();
+            cy.wait(1500);
+        })
+    })
+})

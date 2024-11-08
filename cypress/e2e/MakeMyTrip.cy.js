c96fba5b334dc6e95dc8c353e4051ebc4a16294b
rajatkale7 <rajatkale7@gmail.com>
2024-11-08 20:57:05 +0530

    done with adding gitignore file

--- cypress/e2e/MakeMyTrip.cy.js
+++ cypress/e2e/MakeMyTrip.cy.js
@@ -1,12 +1,15 @@
 describe('this suite is for Searching the flights', () =>{
     beforeEach('Launches the URL', () =>{
-        cy.viewport('macbook-16');
-        cy.visit('https://scholar.google.com/');
+        cy.viewport('macbook-11');
+        cy.wait(200);
+        cy.visit('https://www.google.com');
+        //cy.visit('https://www.sci-hub.ren/');
     })
 
-    it('Searches the flight', () =>{
+    it('Performs Google Scholor search', () =>{
         //nothing
         //cy.scrollTo('bottom');
+        //cy.xpath("//input[@id='gs_hdr_tsi']").type('Agile testing QA{enter}', {delay:100});
         
     })
 })
